77398db6d48f3441ed689ea0f66c423cf6f9c9ee
Omar Chehab <omarchehab98@gmail.com>
2017-04-02 04:49:40 -0400

    prologue audio and transition to chapter 1

--- Assets/Prologue.cs
+++ Assets/Prologue.cs
@@ -1,30 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
-public class Chapter1 : MonoBehaviour {
+public class Prologue : MonoBehaviour {
 	public Transform player;
 	public CloudCluster cloudCluster;
 	public EyeShutter eyes;
 	public GameObject narration;
 	public GameObject track;
 	void Start () {
-		for (int i = 1; i <= 200; i++) {
-			int count = 15;
-			Vector3 position = new Vector3(0, i * -200, 0);
-			int radius = 500;
-			int deltaY = 100;
-			cloudCluster.Spawn(count, position, radius, deltaY);
-		}
+		int count = 50;
+		Vector3 position = new Vector3(70, 600, 10);
+		int radius = 500;
+		int deltaY = 50;
+		cloudCluster.Spawn(count, position, radius, deltaY);
 		StartCoroutine("Go");
 	}
 
 	IEnumerator Go() {
 		yield return new WaitForSeconds(3);
 		narration.SetActive(true);
-		yield return new WaitForSeconds(16);
+		yield return new WaitForSeconds(14);
 		eyes.Open();
-		yield return new WaitForSeconds(45);
+		yield return new WaitForSeconds(2);
+		track.SetActive(true);
+		yield return new WaitForSeconds(59);
 		eyes.Close();
 		yield return new WaitForSeconds(3);
+		Application.LoadLevel("Chapter 1");
 	}
 
 	
