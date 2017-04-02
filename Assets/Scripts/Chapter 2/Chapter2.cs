ec91a03be6da79bdb6abea05a0ea6aac3e8a86d3
Omar Chehab <omarchehab98@gmail.com>
2017-04-02 06:32:32 -0400

    chapter 2 audio and transition to chapter 3

---
+++ Assets/Scripts/Chapter 2/Chapter2.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+public class Chapter2 : MonoBehaviour {
+	public Transform player;
+	public RigidbodyFirstPersonController controller;
+	public CloudCluster cloudCluster;
+	public EyeShutter eyes;
+	public GameObject narration;
+	public GameObject track;
+	void Start () {
+		int count = 50;
+		Vector3 position = new Vector3(70, 1400, 10);
+		int radius = 500;
+		int deltaY = 50;
+		cloudCluster.Spawn(count, position, radius, deltaY);
+		StartCoroutine("Go");
+	}
+
+	IEnumerator Go() {
+		controller.enabled = false;
+		yield return new WaitForSeconds(3);
+		narration.SetActive(true);
+		yield return new WaitForSeconds(18.5f);
+		controller.enabled = true;
+		eyes.Open();
+		yield return new WaitForSeconds(2);
+		track.SetActive(true);
+	}
+
+	public void End (BaseEventData e) {
+		eyes.Close();
+		track.SetActive(false);
+		StartCoroutine("ChangeLevel");
+	}
+
+	IEnumerator ChangeLevel () {
+		yield return new WaitForSeconds(3);
+		Application.LoadLevel("Chapter 3");
+	}
+
+	
+}
---
+++ Assets/Scripts/Chapter 2/DiamondCollection.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DiamondCollection : MonoBehaviour {
+	public EventTrigger.TriggerEvent onCollectAll;
+	int count = 0;
+	bool calledBack = false;
+	
+	void Update () {
+		if (!calledBack && count == 3) {
+			calledBack = true;
+			BaseEventData e = new BaseEventData(EventSystem.current);
+			onCollectAll.Invoke(e);
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Orb") {
+			// Collided with orb
+			Destroy(other.gameObject);
+			count += 1;
+		}
+	}
+}
