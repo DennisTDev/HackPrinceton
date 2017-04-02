15f9272c99e0c897fd1c34e980b4352bcefdd0ec
Kayla Pandza <pandzakayla@gmail.com>
2017-04-02 04:35:23 -0400

    This is the updated map for Chapter Two.

---
+++ Assets/ChapterTwoMain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChapterTwoMain : MonoBehaviour {
+
+	public void onTriggerEnter(Collider info){
+		if (info.name == "diamond1") {
+			Debug.Log ("Diamond picked up"); 
+		}
+	}
+
+}
---
+++ Assets/Scripts/Chapter2Main.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter2Main : MonoBehaviour {
+	int count = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (count == 3) {
+			// Found 3 orbs
+		}
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Orb") {
+			// Collided with orb
+			Destroy(other.gameObject);
+			count += 1;
+		}
+	}
+}
